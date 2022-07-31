1fc84fd20ac92626098a95c1afe33a5af5411948
albertomoiseis <contatoalbertomoiseis@gmail.com>
2022-07-31 17:55:02 -0300

    first commit

---
+++ origamid/01-basico/01-variaveis/script.js
@@ -0,0 +1,15 @@
+var nome = "alberto";
+var idade = 30;
+var possuiFaculdade = true;
+var cidade = "Patos";
+var estado = "PB";
+console.log(nome, idade, possuiFaculdade, cidade, estado)
+
+var preco = 100
+var totalComprado = 10;
+var totalPreco = totalComprado * preco;
+
+console.log(totalPreco)
+
+var sobreNome = nome + " Moiseis"
+console.log(sobreNome)
---
+++ origamid/01-basico/02-tipos-dados/script.js
@@ -0,0 +1,16 @@
+var nome = "alberto";
+var idade = 30;
+var possuiFaculdade = true;
+var time;
+var comida = null;
+var simbolo = Symbol();
+var novoArray = []
+var novoObjeto = {}
+print(typeof(nome));
+print(typeof(idade));
+print(typeof(possuiFaculdade));
+print(typeof(time));
+print(typeof(comida));
+print(typeof(simbolo));
+print(typeof(novoObjeto))
+print(typeof(novoArray))
