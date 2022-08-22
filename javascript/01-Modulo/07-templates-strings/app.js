302eb09f8d0a3d077f014e731553d27d9f1b86a6
albertomoiseis <contatoalbertomoiseis@gmail.com>
2022-08-22 14:30:48 -0300

    Trabalhandos Templates Strings

---
+++ javascript/01-Modulo/07-templates-strings/app.js
@@ -0,0 +1,29 @@
+// inteiros e decimais
+const radius = 10
+const pi = 3.14
+
+//operadores artiméticos
+const area = pi * radius ** 2;
+
+// ordem de operadores
+const ordem = 5 * (2 + 2) ** 1
+
+// operadoes addition, subtraction, multiplication e division
+let valor = 10
+valor = valor + 1
+console.log(valor);
+valor = valor - 1
+console.log(valor);
+valor = valor * 2
+console.log(valor);
+valor = valor / 4
+console.log(valor);
+valor = valor ** 1
+console.log(valor);
+
+valor = valor % 2;
+console.log(valor);
+
+//NaN - not a number
+console.log('2' * 'nome')
+// concatenação de string com números
