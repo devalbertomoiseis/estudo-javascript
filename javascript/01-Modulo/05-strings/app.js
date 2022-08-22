faae0a853dd78a44b16058f5d2904fafddba2b7e
albertomoiseis <contatoalbertomoiseis@gmail.com>
2022-08-22 14:09:29 -0300

    Trabalhando com números

---
+++ javascript/01-Modulo/05-strings/app.js
@@ -0,0 +1,13 @@
+
+// métodos de uma string
+
+const email = 'silva@gmail.com '
+
+const lastIndexOf = email.lastIndexOf('a')
+console.log(lastIndexOf);
+
+const emailSlice = email.slice(0,5);
+console.log(emailSlice);
+
+const emailReplace = email.replace(' ','.');
+console.log(emailReplace);
