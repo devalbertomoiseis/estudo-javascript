7fcab8e7ee89c3e71356ae8afdb0ab190aea8f2f
albertomoiseis <contatoalbertomoiseis@gmail.com>
2022-08-22 11:47:19 -0300

    Trabalhando com Strings

---
+++ javascript/01-Modulo/04-strings/app.js
@@ -0,0 +1,24 @@
+//strings
+console.log("hello, world");
+
+const email = "joao@gmail.com";
+console.log(email);
+
+// concatenação de strings
+
+const firstName = "silva"
+const lastName = "santos"
+const fullName = firstName + '  ' + lastName;
+console.log(fullName);
+
+// acessando caracteres
+console.log(fullName[0]);
+
+// comprimento de uma strings
+console.log(fullName.length);
+
+// métodos de uma string
+console.log(fullName.toLocaleUpperCase()) // maiúsculo
+console.log(fullName.toLowerCase()) // minúsculo
+console.log(fullName.replace(' ', ',')) // trocar espaço em branco por  virgula
+
